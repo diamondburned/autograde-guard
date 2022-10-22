c3206464834ad17301441256b515e900358365c8
diamondburned <diamond@arikawa-hi.me>
2022-10-22 06:18:13 -0700

    Correct action-deps.nix pinning

--- .nix/action-deps.nix
+++ .nix/action-deps.nix
@@ -1,9 +1,9 @@
 let commit = "ac20a8605b0f79be2d65d995cd347251cd5b984b"; # nixos-22.05
-	src = builtins.fetchTarball {
-		src = "https://github.com/NixOS/nixpkgs/archive/refs/${commit}.tar.gz";
-		sha256 = "87428fc522803d31065e7bce3cf03fe475096631e5e07bbd7a0fde60c4cf25c7";
+	pkgsSrc = builtins.fetchTarball {
+		url = "https://github.com/NixOS/nixpkgs/archive/${commit}.tar.gz";
+		sha256 = "184asc3j4vg0qys10yqlla5h05wfb3jm062lwzsrlnjw2yq7nll3";
 	};
-	pkgs = import src {};
+	pkgs = import pkgsSrc {};
 
 in import ./deps.nix {
 	inherit pkgs;

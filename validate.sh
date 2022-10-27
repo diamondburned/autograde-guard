4017c185fb68796520ca8733c8a7c0cc42139fb2
diamondburned <diamond@arikawa-hi.me>
2022-10-27 05:20:30 -0700

    last_commit_{name,email} -> last_committer_...

--- validate.sh
+++ validate.sh
@@ -44,8 +44,8 @@ main() {
 				url "https://github.com/$orgName/$repo" \
 				tampered true \
 				last_commit "$validate_last_commit_hash" \
-				last_commit_name,omitempty "$validate_last_committer_name" \
-				last_commit_email,omitempty "$validate_last_committer_email" \
+				last_committer_name,omitempty "$validate_last_committer_name" \
+				last_committer_email,omitempty "$validate_last_committer_email" \
 				last_commit_is_verified "$validate_last_commit_is_verified" \
 				last_author_id,omitempty "$validate_last_author_id" \
 				last_author_name,omitempty "$validate_last_author_name" \

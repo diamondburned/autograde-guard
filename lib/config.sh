f9e8852ec021a70ba64e59c56ef89a07da0e3d2e
diamondburned <diamond@arikawa-hi.me>
2022-11-09 13:38:07 -0800

    Allow optional config keys

--- lib/config.sh
+++ lib/config.sh
@@ -2,9 +2,27 @@
 
 ConfigFile="./config.toml"
 
+config::_get() {
+	taplo get -f "$ConfigFile" "${@:2}" "$1"
+}
+
 # config::get(key, opts...)
 config::get() {
-	taplo get -f "$ConfigFile" "${@:2}" "$1"
+	if config::_get "$@" 2> /dev/null; then
+		# Bail if taplo didn't complain. If it did, then we'll have to do
+		# something fun...
+		return 0
+	fi
+
+	# We want to return an empty value if the key does not exist, but we don't
+	# really want to exit 0 on any error. Our best way to do this is to just...
+	# compare stderr.
+	local out=$(config::_get "$@" 2>&1)
+	if [[ "$out" == *"error=no values matched the pattern"* ]]; then
+		return 0
+	fi
+
+	return 1
 }
 
 # config::array_into(array_name, key, opts...)
